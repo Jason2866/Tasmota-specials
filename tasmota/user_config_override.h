f241fef6d14285b91bcc2e5b9e5f54f91c23a6de
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2021-01-20 16:53:29 +0100

    Update user_config_override.h

--- tasmota/user_config_override.h
+++ tasmota/user_config_override.h
@@ -980,6 +980,19 @@ oo     .d8P `88b    ooo   888  `88b.   888   888              888       888   8 
   #define USE_SENDMAIL
   #ifdef ESP32
      #define USE_ESP32MAIL
+     #define MAXVARS 75
+     #define MAXSVARS 15
+     #define MAXFILT 10
+     #define UFSYS_SIZE 8192
+     #define USE_SCRIPT_TASK
+     #define LARGE_ARRAYS
+     #define SCRIPT_LARGE_VNBUFF
+     #define USE_SCRIPT_GLOBVARS
+     #define USE_SCRIPT_SUB_COMMAND
+     #define USE_ANGLE_FUNC
+     #define SCRIPT_FULL_WEBPAGE
+     #define SCRIPT_GET_HTTPS_JP
+     #define USE_GOOGLE_CHARTS
   #endif // ESP32
   #define USE_SML_M
   #define USE_SCRIPT_JSON_EXPORT
