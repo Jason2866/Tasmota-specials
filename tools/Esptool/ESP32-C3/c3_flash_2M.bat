7b77af340b0fc36343cfad86ffe0c9fc8ed4843f
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2021-07-18 11:54:00 +0200

    Create c3_flash_2M.bat

---
+++ tools/Esptool/ESP32-C3/c3_flash_2M.bat
@@ -0,0 +1,1 @@
+.py --chip esp32c3 --baud 460800 --before default_reset --after hard_reset write_flash -z --flash_mode dout --flash_freq 40m --flash_size detect 0x0000 bootloader_dout_40m.bin 0x8000 partitions_2M.bin 0xe000 boot_app0.bin 0x10000 tasmota32c3.bin
