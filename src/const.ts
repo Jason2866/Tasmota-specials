c41bbc9daa08b419469834686f524e445147cc2e
Johann Obermeier <obermeier.johann@googlemail.com>
2021-08-23 16:29:05 +0000

    fix errors

--- src/const.ts
+++ src/const.ts
@@ -1,285 +1,88 @@
-import { toByteArray } from "./util";
-
-export interface Logger {
-  log(msg: string, ...args: any[]): void;
-  error(msg: string, ...args: any[]): void;
-  debug(msg: string, ...args: any[]): void;
+export interface Build {
+  chipFamily: "ESP32" | "ESP8266";
+  improv: boolean;
+  parts: {
+    path: string;
+    offset: number;
+  }[];
 }
 
-export const baudRates = [
-  115200, 128000, 153600, 230400, 460800, 921600, 1500000, 2000000,
-];
-export const FLASH_SIZES = {
-  "512KB": 0x00,
-  "256KB": 0x10,
-  "1MB": 0x20,
-  "2MB": 0x30,
-  "4MB": 0x40,
-  "2MB-c1": 0x50,
-  "4MB-c1": 0x60,
-  "8MB": 0x80,
-  "16MB": 0x90,
-};
-
-export const ESP32_FLASH_SIZES = {
-  "1MB": 0x00,
-  "2MB": 0x10,
-  "4MB": 0x20,
-  "8MB": 0x30,
-  "16MB": 0x40,
-  "32MB": 0x19,
-  "64MB": 0x1a,
-};
-
-export const FLASH_MODES = {
-  qio: 0,
-  qout: 1,
-  dio: 2,
-  dout: 3,
-};
-
-export const FLASH_FREQUENCIES = {
-  "40m": 0,
-  "26m": 1,
-  "20m": 2,
-  "80m": 0xf,
-};
-
-export const DETECTED_FLASH_SIZES = {
-  0x12: "256KB",
-  0x13: "512KB",
-  0x14: "1MB",
-  0x15: "2MB",
-  0x16: "4MB",
-  0x17: "8MB",
-  0x18: "16MB",
-  0x19: "32MB",
-  0x1a: "64MB",
-};
-
-export const getFlashSizes = (chipFamily: ChipFamily) => {
-  switch (chipFamily) {
-    case CHIP_FAMILY_ESP32:
-      return ESP32_FLASH_SIZES;
-    case CHIP_FAMILY_ESP32S2:
-      return ESP32_FLASH_SIZES;
-    case CHIP_FAMILY_ESP32C3:
-      return ESP32_FLASH_SIZES;
-    case CHIP_FAMILY_ESP8266:
-      return FLASH_SIZES;
-    default:
-      return FLASH_SIZES;
-  }
-};
-
-export const FLASH_WRITE_SIZE = 0x400;
-export const STUB_FLASH_WRITE_SIZE = 0x4000;
-export const FLASH_SECTOR_SIZE = 0x1000; // Flash sector size, minimum unit of erase.
-export const ESP_ROM_BAUD = 115200;
-export const ESP32_BOOTLOADER_FLASH_OFFSET = 0x1000;
-export const BOOTLOADER_FLASH_OFFSET = 0x0;
-export const ESP_IMAGE_MAGIC = 0xe9;
-
-export const ESP32_SPI_REG_BASE = 0x3ff42000;
-export const ESP32_SPI_USR_OFFS = 0x1c;
-export const ESP32_SPI_USR1_OFFS = 0x20;
-export const ESP32_SPI_USR2_OFFS = 0x24;
-export const ESP32_SPI_MOSI_DLEN_OFFS = 0x28;
-export const ESP32_SPI_MISO_DLEN_OFFS = 0x2c;
-export const ESP32_SPI_W0_OFFS = 0x80;
-export const ESP32_UART_DATE_REG_ADDR = 0x60000078;
-
-export const ESP32S2_SPI_REG_BASE = 0x3f402000;
-export const ESP32S2_SPI_USR_OFFS = 0x18;
-export const ESP32S2_SPI_USR1_OFFS = 0x1c;
-export const ESP32S2_SPI_USR2_OFFS = 0x20;
-export const ESP32S2_SPI_MOSI_DLEN_OFFS = 0x24;
-export const ESP32S2_SPI_MISO_DLEN_OFFS = 0x28;
-export const ESP32S2_SPI_W0_OFFS = 0x58;
-export const ESP32S2_UART_DATE_REG_ADDR = 0x60000078;
-
-export const ESP32C3_SPI_REG_BASE = 0x60002000;
-export const ESP32C3_SPI_USR_OFFS = 0x18;
-export const ESP32C3_SPI_USR1_OFFS = 0x1c;
-export const ESP32C3_SPI_USR2_OFFS = 0x20;
-export const ESP32C3_SPI_MOSI_DLEN_OFFS = 0x24;
-export const ESP32C3_SPI_MISO_DLEN_OFFS = 0x28;
-export const ESP32C3_SPI_W0_OFFS = 0x58;
-export const ESP32C3_UART_DATE_REG_ADDR = 0x6000007c;
-
-export const ESP8266_SPI_REG_BASE = 0x60000200;
-export const ESP8266_SPI_USR_OFFS = 0x1c;
-export const ESP8266_SPI_USR1_OFFS = 0x20;
-export const ESP8266_SPI_USR2_OFFS = 0x24;
-export const ESP8266_SPI_MOSI_DLEN_OFFS = -1;
-export const ESP8266_SPI_MISO_DLEN_OFFS = -1;
-export const ESP8266_SPI_W0_OFFS = 0x40;
-export const ESP8266_UART_DATE_REG_ADDR = 0x60000078;
-
-export interface SpiFlashAddresses {
-  regBase: number;
-  usrOffs: number;
-  usr1Offs: number;
-  usr2Offs: number;
-  mosiDlenOffs: number;
-  misoDlenOffs: number;
-  w0Offs: number;
+export interface Manifest {
+  name: string;
+  builds: Build[];
 }
 
-export const SYNC_PACKET = toByteArray(
-  "\x07\x07\x12 UUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUU"
-);
-export const CHIP_DETECT_MAGIC_REG_ADDR = 0x40001000;
-// These values for the families are made up; nothing that esptool uses.
-export const CHIP_FAMILY_ESP8266 = 0x8266;
-export const CHIP_FAMILY_ESP32 = 0x32;
-export const CHIP_FAMILY_ESP32S2 = 0x3252;
-export const CHIP_FAMILY_ESP32S3 = 0x3253;
-export const CHIP_FAMILY_ESP32C3 = 0x3223;
-export const CHIP_FAMILY_ESP32C6 = 0x3226;
-export const CHIP_FAMILY_ESP32H2 = 0x3272;
-export type ChipFamily =
-  | typeof CHIP_FAMILY_ESP8266
-  | typeof CHIP_FAMILY_ESP32
-  | typeof CHIP_FAMILY_ESP32S2
-  | typeof CHIP_FAMILY_ESP32S3
-  | typeof CHIP_FAMILY_ESP32C3
-  | typeof CHIP_FAMILY_ESP32C6
-  | typeof CHIP_FAMILY_ESP32H2;
+interface BaseFlashState {
+  state: State;
+  message: string;
+  manifest?: Manifest;
+  build?: Build;
+  chipFamily?: "ESP32" | "ESP8266" | "ESP32-S2" | "Unknown Chip";
+}
 
-export const CHIP_DETECT_MAGIC_VALUES = {
-  0xfff0c101: { name: "ESP8266", family: CHIP_FAMILY_ESP8266 },
-  0x00f01d83: { name: "ESP32", family: CHIP_FAMILY_ESP32 },
-  0x000007c6: { name: "ESP32-S2", family: CHIP_FAMILY_ESP32S2 },
-  0x9: { name: "ESP32-S3", family: CHIP_FAMILY_ESP32S3 },
-  0xeb004136: { name: "ESP32-S3(beta2)", family: CHIP_FAMILY_ESP32S3 },
-  0x6921506f: { name: "ESP32-C3", family: CHIP_FAMILY_ESP32C3 },
-  0x1b31506f: { name: "ESP32-C3", family: CHIP_FAMILY_ESP32C3 },
-  0xca26cc22: { name: "ESP32-H2", family: CHIP_FAMILY_ESP32H2 },
-  0x0da1806f: { name: "ESP32-C6(beta)", family: CHIP_FAMILY_ESP32C6 },
-};
+export interface InitializingState extends BaseFlashState {
+  state: State.INITIALIZING;
+  details: { done: boolean };
+}
 
-export const ESP32_DATAREGVALUE = 0x15122500;
-export const ESP8266_DATAREGVALUE = 0x00062000;
-export const ESP32S2_DATAREGVALUE = 0x500;
+export interface ManifestState extends BaseFlashState {
+  state: State.MANIFEST;
+  details: { done: boolean };
+}
 
-// Commands supported by ESP8266 ROM bootloader
-export const ESP_FLASH_BEGIN = 0x02;
-export const ESP_FLASH_DATA = 0x03;
-export const ESP_FLASH_END = 0x04;
-export const ESP_MEM_BEGIN = 0x05;
-export const ESP_MEM_END = 0x06;
-export const ESP_MEM_DATA = 0x07;
-export const ESP_SYNC = 0x08;
-export const ESP_WRITE_REG = 0x09;
-export const ESP_READ_REG = 0x0a;
+export interface PreparingState extends BaseFlashState {
+  state: State.PREPARING;
+  details: { done: boolean };
+}
 
-export const ESP_ERASE_FLASH = 0xd0;
-export const ESP_ERASE_REGION = 0xd1;
+export interface ErasingState extends BaseFlashState {
+  state: State.ERASING;
+  details: { done: boolean };
+}
 
-export const ESP_SPI_SET_PARAMS = 0x0b;
-export const ESP_SPI_ATTACH = 0x0d;
-export const ESP_CHANGE_BAUDRATE = 0x0f;
-export const ESP_SPI_FLASH_MD5 = 0x13;
-export const ESP_CHECKSUM_MAGIC = 0xef;
-export const ESP_FLASH_DEFL_BEGIN = 0x10;
-export const ESP_FLASH_DEFL_DATA = 0x11;
-export const ESP_FLASH_DEFL_END = 0x12;
+export interface WritingState extends BaseFlashState {
+  state: State.WRITING;
+  details: { bytesTotal: number; bytesWritten: number; percentage: number };
+}
 
-export const ROM_INVALID_RECV_MSG = 0x05;
+export interface FinishedState extends BaseFlashState {
+  state: State.FINISHED;
+}
 
-export const USB_RAM_BLOCK = 0x800;
-export const ESP_RAM_BLOCK = 0x1800;
+export interface ErrorState extends BaseFlashState {
+  state: State.ERROR;
+  details: { error: FlashError; details: string | Error };
+}
 
-// Timeouts
-export const DEFAULT_TIMEOUT = 3000;
-export const CHIP_ERASE_TIMEOUT = 600000; // timeout for full chip erase in ms
-export const MAX_TIMEOUT = CHIP_ERASE_TIMEOUT * 2; // longest any command can run in ms
-export const SYNC_TIMEOUT = 100; // timeout for syncing with bootloader in ms
-export const ERASE_REGION_TIMEOUT_PER_MB = 30000; // timeout (per megabyte) for erasing a region in ms
-export const MEM_END_ROM_TIMEOUT = 50;
+export type FlashState =
+  | InitializingState
+  | ManifestState
+  | PreparingState
+  | ErasingState
+  | WritingState
+  | FinishedState
+  | ErrorState;
 
-/**
- * @name timeoutPerMb
- * Scales timeouts which are size-specific
- */
-export const timeoutPerMb = (secondsPerMb: number, sizeBytes: number) => {
-  let result = Math.floor(secondsPerMb * (sizeBytes / 0x1e6));
-  if (result < DEFAULT_TIMEOUT) {
-    return DEFAULT_TIMEOUT;
+export const enum State {
+  INITIALIZING = "initializing",
+  MANIFEST = "manifest",
+  PREPARING = "preparing",
+  ERASING = "erasing",
+  WRITING = "writing",
+  FINISHED = "finished",
+  ERROR = "error",
+}
+
+export const enum FlashError {
+  FAILED_INITIALIZING = "failed_initialize",
+  FAILED_MANIFEST_FETCH = "fetch_manifest_failed",
+  NOT_SUPPORTED = "not_supported",
+  FAILED_FIRMWARE_DOWNLOAD = "failed_firmware_download",
+  WRITE_FAILED = "write_failed",
+}
+
+declare global {
+  interface HTMLElementEventMap {
+    "state-changed": CustomEvent<FlashState>;
   }
-  return result;
-};
-
-export const getSpiFlashAddresses = (
-  chipFamily: ChipFamily
-): SpiFlashAddresses => {
-  switch (chipFamily) {
-    case CHIP_FAMILY_ESP32:
-      return {
-        regBase: ESP32_SPI_REG_BASE,
-        usrOffs: ESP32_SPI_USR_OFFS,
-        usr1Offs: ESP32_SPI_USR1_OFFS,
-        usr2Offs: ESP32_SPI_USR2_OFFS,
-        mosiDlenOffs: ESP32_SPI_MOSI_DLEN_OFFS,
-        misoDlenOffs: ESP32_SPI_MISO_DLEN_OFFS,
-        w0Offs: ESP32_SPI_W0_OFFS,
-      };
-    case CHIP_FAMILY_ESP32S2:
-      return {
-        regBase: ESP32_SPI_REG_BASE,
-        usrOffs: ESP32_SPI_USR_OFFS,
-        usr1Offs: ESP32_SPI_USR1_OFFS,
-        usr2Offs: ESP32_SPI_USR2_OFFS,
-        mosiDlenOffs: ESP32_SPI_MOSI_DLEN_OFFS,
-        misoDlenOffs: ESP32_SPI_MISO_DLEN_OFFS,
-        w0Offs: ESP32_SPI_W0_OFFS,
-      };
-    case CHIP_FAMILY_ESP32C3:
-      return {
-        regBase: ESP32C3_SPI_REG_BASE,
-        usrOffs: ESP32C3_SPI_USR_OFFS,
-        usr1Offs: ESP32C3_SPI_USR1_OFFS,
-        usr2Offs: ESP32C3_SPI_USR2_OFFS,
-        mosiDlenOffs: ESP32C3_SPI_MOSI_DLEN_OFFS,
-        misoDlenOffs: ESP32C3_SPI_MISO_DLEN_OFFS,
-        w0Offs: ESP32C3_SPI_W0_OFFS,
-      };
-    case CHIP_FAMILY_ESP8266:
-      return {
-        regBase: ESP8266_SPI_REG_BASE,
-        usrOffs: ESP8266_SPI_USR_OFFS,
-        usr1Offs: ESP8266_SPI_USR1_OFFS,
-        usr2Offs: ESP8266_SPI_USR2_OFFS,
-        mosiDlenOffs: ESP8266_SPI_MOSI_DLEN_OFFS,
-        misoDlenOffs: ESP8266_SPI_MISO_DLEN_OFFS,
-        w0Offs: ESP8266_SPI_W0_OFFS,
-      };
-    default:
-      return {
-        regBase: -1,
-        usrOffs: -1,
-        usr1Offs: -1,
-        usr2Offs: -1,
-        mosiDlenOffs: -1,
-        misoDlenOffs: -1,
-        w0Offs: -1,
-      };
-  }
-};
-
-export const getUartDateRegAddress = (chipFamily: ChipFamily): number => {
-  // Additional chips like S3, C3, C6 have different addresses
-  switch (chipFamily) {
-    case CHIP_FAMILY_ESP32:
-      return ESP32_UART_DATE_REG_ADDR;
-    case CHIP_FAMILY_ESP32S2:
-      return ESP32S2_UART_DATE_REG_ADDR;
-    case CHIP_FAMILY_ESP32C3:
-      return ESP32C3_UART_DATE_REG_ADDR;
-    case CHIP_FAMILY_ESP8266:
-      return ESP8266_UART_DATE_REG_ADDR;
-    default:
-      return -1;
-  }
-};
+}
--- src/util.ts
+++ src/util.ts
@@ -1,112 +1,43 @@
-/**
- * @name slipEncode
- * Take an array buffer and return back a new array where
- * 0xdb is replaced with 0xdb 0xdd and 0xc0 is replaced with 0xdb 0xdc
- */
-export const slipEncode = (buffer: number[]): number[] => {
-  let encoded: number[] = [];
-  for (let byte of buffer) {
-    if (byte == 0xdb) {
-      encoded = encoded.concat([0xdb, 0xdd]);
-    } else if (byte == 0xc0) {
-      encoded = encoded.concat([0xdb, 0xdc]);
-    } else {
-      encoded.push(byte);
-    }
-  }
-  return encoded;
-};
+import {
+  CHIP_FAMILY_ESP32,
+  CHIP_FAMILY_ESP32S2,
+  CHIP_FAMILY_ESP8266,
+  ESPLoader,
+} from "esp-web-flasher";
 
-/**
- * @name toByteArray
- * Convert a string to a byte array
- */
-export const toByteArray = (str: string): number[] => {
-  let byteArray: number[] = [];
-  for (let i = 0; i < str.length; i++) {
-    let charcode = str.charCodeAt(i);
-    if (charcode <= 0xff) {
-      byteArray.push(charcode);
-    }
-  }
-  return byteArray;
-};
-
-export const pack = (format: string, ...data: number[]) => {
-  // let format = args[0];
-  let pointer = 0;
-  // let data = args.slice(1);
-  if (format.replace(/[<>]/, "").length != data.length) {
-    throw new Error("Pack format to Argument count mismatch");
-  }
-  let bytes: number[] = [];
-  let littleEndian = true;
-
-  const pushBytes = (value: number, byteCount: number) => {
-    for (let i = 0; i < byteCount; i++) {
-      if (littleEndian) {
-        bytes.push((value >> (i * 8)) & 0xff);
-      } else {
-        bytes.push((value >> ((byteCount - i) * 8)) & 0xff);
-      }
-    }
-  };
-
-  for (let i = 0; i < format.length; i++) {
-    if (format[i] == "<") {
-      littleEndian = true;
-    } else if (format[i] == ">") {
-      littleEndian = false;
-    } else if (format[i] == "B") {
-      pushBytes(data[pointer], 1);
-      pointer++;
-    } else if (format[i] == "H") {
-      pushBytes(data[pointer], 2);
-      pointer++;
-    } else if (format[i] == "I") {
-      pushBytes(data[pointer], 4);
-      pointer++;
-    } else {
-      throw new Error(`Unhandled character "${format[i]}" in pack format`);
-    }
-  }
-
-  return bytes;
-};
-
-export const unpack = (format: string, bytes: number[]) => {
-  let pointer = 0;
-  let data = [];
-  for (let c of format) {
-    if (c == "B") {
-      data.push(bytes[pointer] & 0xff);
-      pointer += 1;
-    } else if (c == "H") {
-      data.push((bytes[pointer] & 0xff) | ((bytes[pointer + 1] & 0xff) << 8));
-      pointer += 2;
-    } else if (c == "I") {
-      data.push(
-        (bytes[pointer] & 0xff) |
-          ((bytes[pointer + 1] & 0xff) << 8) |
-          ((bytes[pointer + 2] & 0xff) << 16) |
-          ((bytes[pointer + 3] & 0xff) << 24)
-      );
-      pointer += 4;
-    } else {
-      throw new Error(`Unhandled character "${c}" in unpack format`);
-    }
-  }
-  return data;
-};
-
-export const toHex = (value: number, size = 2) => {
-  let hex = value.toString(16).toUpperCase();
-  if (hex.startsWith("-")) {
-    return "-0x" + hex.substring(1).padStart(size, "0");
-  } else {
-    return "0x" + hex.padStart(size, "0");
+export const getChipFamilyName = (esploader: ESPLoader) => {
+  switch (esploader.chipFamily) {
+    case CHIP_FAMILY_ESP32:
+      return "ESP32";
+    case CHIP_FAMILY_ESP8266:
+      return "ESP8266";
+    case CHIP_FAMILY_ESP32S2:
+      return "ESP32-S2";
+    default:
+      return "Unknown Chip";
   }
 };
 
-export const sleep = (ms: number) =>
-  new Promise((resolve) => setTimeout(resolve, ms));
+export const sleep = (time: number) =>
+  new Promise((resolve) => setTimeout(resolve, time));
+
+export const fireEvent = <Event extends keyof HTMLElementEventMap>(
+  eventTarget: EventTarget,
+  type: Event,
+  // @ts-ignore
+  detail?: HTMLElementEventMap[Event]["detail"],
+  options?: {
+    bubbles?: boolean;
+    cancelable?: boolean;
+    composed?: boolean;
+  }
+): void => {
+  options = options || {};
+  const event = new CustomEvent(type, {
+    bubbles: options.bubbles === undefined ? true : options.bubbles,
+    cancelable: Boolean(options.cancelable),
+    composed: options.composed === undefined ? true : options.composed,
+    detail,
+  });
+  eventTarget.dispatchEvent(event);
+};
