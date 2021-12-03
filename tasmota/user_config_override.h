48342022a611ac2ce85634087572de6c2300df97
Charles <hallard04@free.fr>
2021-12-03 11:26:29 +0100

    use energy sensor on thremostat

--- tasmota/user_config_override.h
+++ tasmota/user_config_override.h
@@ -1900,6 +1900,7 @@ Thermostat
 //#define USE_HRXL                                 // Add support for MaxBotix HRXL-MaxSonar ultrasonic range finders (+0k7)
 
 // -- Power monitoring sensors --------------------
+#define USE_ENERGY_SENSOR
 //#define USE_ENERGY_MARGIN_DETECTION              // Add support for Energy Margin detection (+1k6 code)
   #define USE_ENERGY_POWER_LIMIT                 // Add additional support for Energy Power Limit detection (+1k2 code)
   #define USE_PZEM004T                             // Add support for PZEM004T Energy monitor (+2k code)
