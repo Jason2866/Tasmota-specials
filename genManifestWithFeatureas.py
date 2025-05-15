871d403ff65d189847b40e25337deb386e35bf0b
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2025-05-15 15:07:32 +0200

    Update genManifestWithFeatureas.py

--- genManifestWithFeatureas.py
+++ genManifestWithFeatureas.py
@@ -64,9 +64,9 @@ def add_features_from_map(infile):
     # We need a repo, which holds all the map.gz files too
     # This is perhaps not the final solution
     if "tasmota32" in file_name:
-        url = ' https://github.com/arendst/Tasmota-firmware/raw/firmware/tasmota32/'+file_name'.map.gz'
+        url = ' https://github.com/arendst/Tasmota-firmware/raw/firmware/tasmota32/'+file_name+'.map.gz'
     else:
-        url = ' https://github.com/arendst/Tasmota-firmware/raw/firmware/tasmota/'+file_name'.map.gz'
+        url = ' https://github.com/arendst/Tasmota-firmware/raw/firmware/tasmota/'+file_name+'.map.gz'
     r = requests.get(url)
     if(r):
         # print("Found map for ",infile)
